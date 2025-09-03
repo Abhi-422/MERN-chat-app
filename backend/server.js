8b61feb7dae95ca33ecc81f1c85e24645e002cc7
Abhi-422 <abhishekpatange422@gmail.com>
2025-09-04 00:08:13 +0530

    updated the routes with one ping route to resolve he issue of server sleeping

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,9 @@ app.use(cors({
 app.use(express.json()); // to parse the incoming requests with JSON payload from req.body
 app.use(cookieParser());
 
+app.get("/ping",(req,res)=>{
+  res.json({status:"ok"});
+})
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
